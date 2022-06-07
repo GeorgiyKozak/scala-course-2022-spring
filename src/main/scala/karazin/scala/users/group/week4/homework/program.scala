7b9d8ac781a73dbbe4eccac6baf48f7a0006a947
Georgiy Kozak <zhora.kozak2002@gmail.com>
2022-06-08 01:17:02 +0300

    Week-4 - Fix the code in program and services

--- src/main/scala/karazin/scala/users/group/week4/homework/program.scala
+++ src/main/scala/karazin/scala/users/group/week4/homework/program.scala
@@ -1,37 +1,69 @@
 /*
-
 Task:
   • fix the code to make it compilable
   • use at least 2 execution contexts:
     • one for a `for comprehension`
     • at least one for `getComments`, `getLikes`, `getShares`
   • write tests
+*/
 
 package karazin.scala.users.group.week4.homework
 
 import java.util.UUID
-import scala.concurrent.Future
+import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutorService, Future}
+import scala.concurrent.duration.DurationInt
 import scala.util.Success
 import scala.util.Failure
+import java.util.concurrent.Executors
+import karazin.scala.users.group.week4.homework.model.*
+import karazin.scala.users.group.week4.homework.services.*
 
-import karazin.scala.users.group.week4.homework.model._
+import scala.language.postfixOps
 
+object program extends App :
 
-object program:
+  given globalContext: ExecutionContext = ExecutionContext.global
+
+  given singleThreadPoolContext: ExecutionContextExecutorService =
+    ExecutionContext.fromExecutorService(Executors.newSingleThreadExecutor)
+
+  given fixedThreadPoolContext: ExecutionContextExecutorService =
+    ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(2))
+
   // Make sure that the result type is exactly `Future[List[PostView]]`
   // not `Future[List[Future[PostView]]]`
-  def getPostsViews(): Future[List[PostView]] = ???
+  def getPostsViews(): Future[List[PostView]] = {
+    val userProfile = getUserProfile()(using singleThreadPoolContext)
+    userProfile onComplete {
+      case Success(profile) => {
+        getPosts(profile.userId)(using fixedThreadPoolContext) flatMap (posts: List[Post] =>
+          posts foldLeft (List[PostView]()) {(acc, el) =>
+          getPostView(el)(using globalContext) onComplete {
+            case Success (postView: PostView) => postView :: acc
+            case Failure (error) => acc
+          }
+        })(using fixedThreadPoolContext)
+      }
+      case Failure(error) => error
+    }
+  }
 
-  def getPostView(post: Post): Future[PostView] = 
-    
-    val getCommentsService  = getComments(post.postId)
-    val getLikesService     = getLikes(post.postId)
-    val getSharesService    = getShares(post.postId)
-    
+  def getPostView(post: Post)(using ec: ExecutionContext): Future[PostView] =
+    println(s"Main thread: ${Thread.currentThread().getName}")
+
+    val getCommentsService = getComments(post.postId)(using singleThreadPoolContext)
+    val getLikesService = getLikes(post.postId)(using fixedThreadPoolContext)
+    val getSharesService = getShares(post.postId)(using fixedThreadPoolContext)
+
     for
-      comments  ← getCommentsService
-      likes     ← getLikesService
-      shares    ← getSharesService
+      comments ← getCommentsService
+      likes ← getLikesService
+      shares ← getSharesService
     yield PostView(post, comments, likes, shares)
-    
- */
+
+  end getPostView
+
+  Await.result(getPostView(Post(UUID.randomUUID(), UUID.randomUUID()))(using globalContext), 20 seconds)
+
+  singleThreadPoolContext.shutdown()
+  fixedThreadPoolContext.shutdown()
